b887252296c18ab4626ab821ab07e28d787dd133
Tumaini Muriithi <wendomuriithi@gmail.com>
2023-12-10 18:20:29 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens_ls = [n for n in num_list if n%2 == 0]
+    return evens_ls
 
 def make_exclamation(sentence_list):
-    pass
+    exclam_ls = [x + "!" for x in sentence_list]
+    return exclam_ls
+    
